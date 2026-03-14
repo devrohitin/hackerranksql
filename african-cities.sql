cbb1675f52ff239fad161ca4a2119d98259a6607
devrohitin <rkumar72@lenovo.com>
2026-03-14 16:35:51 +0530

    Add SQL query to select African city names

---
+++ african-cities.sql
@@ -0,0 +1,7 @@
+/* https://www.hackerrank.com/challenges/african-cities/problem?isFullScreen=true */
+
+SELECT CITY.NAME
+FROM CITY
+JOIN COUNTRY
+ON CITY.CountryCode = COUNTRY.Code 
+WHERE CONTINENT='Africa';

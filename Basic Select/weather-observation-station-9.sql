586e71de08d34795b2e493d4c42f0e13914d70b2
devrohitin <rkumar72@lenovo.com>
2026-03-19 11:34:54 +0530

    Add SQL query to select distinct cities

--- Basic Select/weather-observation-station-9.sql
+++ Basic Select/weather-observation-station-9.sql
@@ -1,1 +1,4 @@
-
+/* https://www.hackerrank.com/challenges/weather-observation-station-9/problem?isFullScreen=true */
+SELECT DISTINCT CITY
+FROM STATION 
+WHERE UPPER(SUBSTR(CITY, 1, 1)) NOT IN ('A', 'E', 'I', 'O', 'U');

7ceea9576454919ad97b6dd30d47d7c8ad87c26b
devrohitin <rkumar72@lenovo.com>
2026-03-19 11:33:09 +0530

    Create weather-observation-station-12.sql

---
+++ Basic Select/weather-observation-station-12.sql
@@ -0,0 +1,8 @@
+/* 
+https://www.hackerrank.com/challenges/weather-observation-station-12/problem?isFullScreen=true
+
+*/
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE (UPPER(SUBSTR(CITY,1,1)) NOT IN ('A','E','I','O','U')) OR (UPPER(SUBSTR (CITY,LENGTH(CITY),1)) NOT IN ('A','E','I','O','U'));

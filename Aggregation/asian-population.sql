2eb8e39a9467e76f1679deb25b50f212201f8e0f
devrohitin <rkumar72@lenovo.com>
2026-03-19 06:44:48 +0000

    change file location

---
+++ Aggregation/asian-population.sql
@@ -0,0 +1,7 @@
+/* https://www.hackerrank.com/challenges/asian-population/problem?isFullScreen=true  */
+
+SELECT SUM(CITY.POPULATION)
+FROM CITY
+JOIN COUNTRY
+ON CITY.CountryCode = COUNTRY.Code
+WHERE COUNTRY.CONTINENT='Asia';

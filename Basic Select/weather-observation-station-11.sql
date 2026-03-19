70a688e8f3ee6aefa731bdeafe76e68bacde3605
devrohitin <rkumar72@lenovo.com>
2026-03-19 11:30:26 +0530

    Update weather-observation-station-11.sql query
    Add a comment with the problem link and modify the query to select distinct cities based on specific conditions.

--- Basic Select/weather-observation-station-11.sql
+++ Basic Select/weather-observation-station-11.sql
@@ -1,1 +1,5 @@
+/*https://www.hackerrank.com/challenges/weather-observation-station-11/problem?isFullScreen=true  */
 
+SELECT DISTINCT CITY
+FROM STATION
+WHERE (UPPER(SUBSTR(CITY,1,1)) NOT IN ('A','E','I','O','U')) OR (UPPER(SUBSTR (CITY,LENGTH(CITY),1)) NOT IN ('A','E','I','O','U'));

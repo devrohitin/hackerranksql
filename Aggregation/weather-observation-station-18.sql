95e4f88cefa4eeb8b94da45f5cc37d132c154e95
devrohitin <rkumar72@lenovo.com>
2026-03-19 20:30:51 +0530

    Create weather-observation-station-18.sql

---
+++ Aggregation/weather-observation-station-18.sql
@@ -0,0 +1,5 @@
+/* https://www.hackerrank.com/challenges/weather-observation-station-18/problem?isFullScreen=true  */
+
+SELECT 
+    DECIMAL(ROUND(ABS(MIN(LAT_N) - MAX(LAT_N)) + ABS(MIN(LONG_W) - MAX(LONG_W)),4),12,4)
+FROM STATION;

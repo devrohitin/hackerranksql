1a974e092a8cc99ea1044bab81605327708005b5
devrohitin <rkumar72@lenovo.com>
2026-03-19 18:49:34 +0530

    Create weather-observation-station-13.sql

---
+++ Aggregation/weather-observation-station-13.sql
@@ -0,0 +1,5 @@
+/* https://www.hackerrank.com/challenges/weather-observation-station-13/problem?isFullScreen=true */
+
+SELECT DECIMAL(SUM(lAT_N),12,4)
+FROM STATION
+WHERE LAT_N BETWEEN 38.7880 AND 137.2345;

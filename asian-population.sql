5a144437ef4920b0caf6d2e7eb74dc97142263af
devrohitin <rkumar72@lenovo.com>
2026-03-19 06:51:15 +0000

    move some file to foler Basic Select

--- asian-population.sql
+++
@@ -1,7 +0,0 @@
-/* https://www.hackerrank.com/challenges/asian-population/problem?isFullScreen=true  */
-
-SELECT SUM(CITY.POPULATION)
-FROM CITY
-JOIN COUNTRY
-ON CITY.CountryCode = COUNTRY.Code
-WHERE COUNTRY.CONTINENT='Asia';
--- average-population-of-each-continent.sql
+++
@@ -1,7 +0,0 @@
-/* https://www.hackerrank.com/challenges/average-population-of-each-continent/problem?isFullScreen=true */
-
-SELECT COUNTRY.Continent, FLOOR(AVG(CITY.Population))
-FROM CITY
-JOIN COUNTRY
-ON CITY.CountryCode=COUNTRY.Code
-GROUP BY COUNTRY.Continent;

411fd3381ee3230fcd714f1e7d38d58cb9505a4e
devrohitin <rkumar72@lenovo.com>
2026-03-19 19:07:56 +0530

    Create weather-observation-station-15.sql

---
+++ Aggregation/weather-observation-station-15.sql
@@ -0,0 +1,7 @@
+/* https://www.hackerrank.com/challenges/weather-observation-station-15/problem?isFullScreen=true */
+
+SELECT DECIMAL(ROUND(LONG_W,4),12,4) 
+FROM STATION
+WHERE LAT_N = (
+SELECT MAX(LAT_N) FROM STATION WHERE LAT_N<137.2345
+);

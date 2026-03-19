15e4fc813799a51ca2de687d775f2dea39672c0b
devrohitin <rkumar72@lenovo.com>
2026-03-19 07:26:17 +0000

    added 5 file

---
+++ Aggregation/average-population.sql
@@ -0,0 +1,5 @@
+/* https://www.hackerrank.com/challenges/average-population/problem?isFullScreen=true */
+
+
+SELECT INT(AVG(POPULATION)) 
+FROM CITY;
---
+++ Aggregation/revising-aggregations-the-count-function.sql
@@ -0,0 +1,5 @@
+/* https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem?isFullScreen=true */
+
+SELECT COUNT(*)
+FROM CITY
+WHERE POPULATION >100000;
